bbf963320a23a9aae834be0870ece2db9dcc035b
CG Programm <rohinipatil0907@gmail.com>
2022-08-27 11:24:15 +0530

    no message

---
+++ Code-1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,a=1,b=1,c;
+    printf("inter the value of n:");
+    scanf("%d",a);
+    printf("%d,b")
+    for(i=3;i<=n,i++)
+    {
+        c=a+b;
+        printf("%d,c")
+        a=b
+        b=c
+        c=0
+    }
+    return0;
+}
